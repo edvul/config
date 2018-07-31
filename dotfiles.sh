979548703b2b10ad783802f9fec16c5a5a216db3
Ed Vul <edwardvul@gmail.com>
2018-07-31 12:02:46 -0400

    switch to symlinks

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,24 +4,37 @@
 
 # git pull origin master;
 
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms dotfiles/. ~;
-	source ~/.bash_profile;
-}
+# TODO: swtich to gnu stow for more elegant symlinks?
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+shopt -s extglob
+for f in dotfiles/.!(|.); 
+do 
+	# echo $f;
+	rm ~/`basename $f`; 
+	ln -s `pwd`/${f} ~/`basename $f`;
+done
+
+
+
+
+# function doIt() {
+# 	rsync --exclude ".git/" \
+# 		--exclude ".DS_Store" \
+# 		--exclude ".osx" \
+# 		--exclude "bootstrap.sh" \
+# 		--exclude "README.md" \
+# 		--exclude "LICENSE-MIT.txt" \
+# 		-avh --no-perms dotfiles/. ~;
+# 	source ~/.bash_profile;
+# }
+
+# if [ "$1" == "--force" -o "$1" == "-f" ]; then
+# 	doIt;
+# else
+# 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+# 	echo "";
+# 	if [[ $REPLY =~ ^[Yy]$ ]]; then
+# 		doIt;
+# 	fi;
+# fi;
+# unset doIt;
